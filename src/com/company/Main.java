6f3589188621f52c0d4a114e0f80f2080e64b251
rahullathar123 <rahullathar123@gmail.com>
2017-12-05 22:37:45 -0500

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+import sun.jvm.hotspot.oops.OopUtilities;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+
+        Outlander outlander = new Outlander(36);
+        outlander.accelerate(30);
+    }
+}
